91527280cbaf9c03ced3993fa45b2b7c3dfdae29
Pratik22jdv <prjadhav2209@gmail.com>
2023-04-12 13:42:00 +0530

    First commit

---
+++ e-matdaan-dapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <div className="Navbar">
+        <Navbar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
