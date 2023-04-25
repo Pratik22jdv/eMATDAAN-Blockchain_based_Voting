87a1f331d4b86cb866ac7932f9287d74652312ca
Pratik22jdv <prjadhav2209@gmail.com>
2023-04-25 11:17:22 +0530

    Opens modal with user details

--- eMATDAAN_Part-1/e-matdaan-dapp/src/components/Admin/Admin.js
+++ eMATDAAN_Part-1/e-matdaan-dapp/src/components/Admin/Admin.js
@@ -13,12 +13,30 @@ import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import { CircularProgress } from "@mui/material";
 import toast, { Toaster } from "react-hot-toast";
 import { Button, Typography } from "@mui/material";
+import {Box, Modal} from "@mui/material";
+import { Link } from "react-router-dom";
 
 
 
 function Admin({ user }) {
     const [userList, setUserList] = useState([]);
     const [isFetching, setFetching] = useState({});
+    const [open, setOpen] = useState(false);
+    const [userData, setUserData] = useState(null);
+
+
+    const style = {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "wrap-content",
+        bgcolor: "background.paper",
+        border: "2px solid #000",
+        boxShadow: 24,
+        p: 4,
+      };
+
 
     // const notifySuccess = (message) => toast.success(message);
 
@@ -79,6 +97,17 @@ function Admin({ user }) {
     //     createData('Gingerbread', 356, 16.0, 49, 3.9),
     // ];
 
+    
+
+    //Modal open with user details
+    const handleOpen = (user) => {
+        setUserData(user);
+        setOpen(true);
+    };
+
+    const handleClose = () => setOpen(false);
+
+
     const setUserApproved = async (userObj) => {
         console.log(userObj);
         setFetching({ ...isFetching, user: userObj._id });
@@ -112,54 +141,109 @@ function Admin({ user }) {
 
 
     useEffect(() => {
-        fetchUserList(user);
+        fetchUserList();
     }, []);
 
     return (
         <div style={{ margin: "20px", marginTop: "10px" }}>
-            {userList.length > 0 ? (<TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Email</TableCell>
-                            <TableCell >User&nbsp;Name</TableCell>
-                            <TableCell >Aadhar&nbsp;Number</TableCell>
-                            <TableCell >Approved&nbsp;</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {userList.map((userObj) => (
-                            <TableRow
-                                key={userObj.email}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <StyledTableCell component="th" scope="row">
-                                    {userObj.email}
-                                </StyledTableCell>
-                                <StyledTableCell >{userObj.firstName + " " + userObj.lastName}</StyledTableCell>
-                                <StyledTableCell >{userObj.aadharNumber}</StyledTableCell>
-                                <StyledTableCell >
-                                    {isFetching.user == userObj._id ?
-                                        (<CircularProgress />) :
-                                        (<>
-                                            {
-                                                userObj.approved ?
-                                                    (<Button variant="contained" disabled onClick={() => setUserApproved(userObj)}>
-                                                        Approved
-                                                    </Button>) :
-                                                    (<Button variant="contained" disabled onClick={() => setUserApproved(userObj)}>
-                                                        Approve
-                                                    </Button>)
-                                            }
-                                        </>
-                                        )
-                                    }
-                                </StyledTableCell>
+            {userList.length > 0 ?
+                (<><TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Email</TableCell>
+                                <TableCell >User&nbsp;Name</TableCell>
+                                <TableCell >Aadhar&nbsp;Number</TableCell>
+                                <TableCell >Approved&nbsp;</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>) :
+                        </TableHead>
+                        <TableBody>
+                            {userList.map((userObj) => (
+                                <TableRow
+                                    key={userObj.email}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <StyledTableCell onClick={()=>{handleOpen(userObj)}} component="th" scope="row">
+                                        <Link>{userObj.email}</Link>
+                                    </StyledTableCell>
+                                    <StyledTableCell >{userObj.firstName + " " + userObj.lastName}</StyledTableCell>
+                                    <StyledTableCell >{userObj.aadharNumber}</StyledTableCell>
+                                    <StyledTableCell >
+                                        {isFetching.user == userObj._id ?
+                                            (<CircularProgress />) :
+                                            (<>
+                                                {
+                                                    userObj.approved ?
+                                                        (<Button variant="contained" disabled >
+                                                            Approved
+                                                        </Button>) :
+                                                        (<Button variant="contained" onClick={() => setUserApproved(userObj)}>
+                                                            Approve
+                                                        </Button>)
+                                                }
+                                            </>
+                                            )
+                                        }
+                                    </StyledTableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box sx={style}>
+                            <div>
+                                <Typography id="modal-modal-title" variant="h4" component="h2">
+                                    User Details
+                                </Typography>
+
+                                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                    <span className="me-4">Name: {" "}{userData?.firstName? userData?.firstName: null}{" "}
+                                    {userData?.lastName ?userData.lastName : " "}</span>
+                                </Typography>
+
+                                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                    <span className="me-4">Aadhar Number : {userData?.aadharNumber}</span>
+                                </Typography>
+
+                                <Typography>
+                                    <span className="me-4">
+                                        Date Of Birth:
+                                        {userData?.userDob ? userData.userDob : "Not Provided"}
+                                    </span>
+                                </Typography>
+
+                                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                    Address:
+                                    {userData?.address1 + " " + userData?.address2}
+                                </Typography>
+
+                                <Typography>
+                                    District:{" "}
+                                    {userData?.userDistrict ? userData.userDistrict : "" + " "}
+                                </Typography>
+
+                                <Typography>
+                                    State:{userData?.userState ? userData.userState : "" + " "}
+                                </Typography>
+
+                                <img
+                                    src={userData?.aadharImage}
+                                    style={{ width: "400px", height: "300px" }}
+                                    alt="userImage"
+                                />
+                            </div>
+                        </Box>
+                    </Modal>
+                </>
+
+                ) :
                 (<div style={{ width: "80px", margin: "auto", marginTop: "20%", backgroundColor: "white", padding: "20px", borderRadius: "20px" }}>
                     <CircularProgress />
                 </div>)
